5930bba93ffd582360caf6ad3bea25015b39fe60
Samir613 <70025371+Samir613@users.noreply.github.com>
2020-11-04 16:54:00 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var speed,weight,thickness,bullet,wall,damage;
+
+function setup() {
+  createCanvas(1000, 400);
+  bullet = createSprite(100,200,50,20);
+  wall = createSprite(900,200,30,300);
+  bullet.velocityX = 10;
+  bullet.shapeColor ="white";
+  wall.shapeColor ="brown";
+  speed = Math.floor(random(223,321));
+  weight = Math.floor(random(30,52));
+  thickness = Math.floor(random(22,83));
+}
+
+function draw() {
+  background("yellow");
+ 
+  drawSprites();
+  
+  collide(bullet,wall);
+  
+  textSize(21);
+  fill("black");
+  text("Damage : < 10 then the Wall is strong",200,340);
+  text("Damage : > 10 then the Wall is weak",200,360);
+  fill("red");
+  textSize(25);
+  text("DAMAGE IS : "+Math.floor(damage),30,40);
+}
+function damages(){
+  damage = (0.5 * weight * speed *speed)/(thickness*thickness*thickness);
+  console.log(damage);
+
+  if(damage < 10 ){
+  
+    bullet.shapeColor ="lightgreen";
+  
+  }
+
+  else{
+
+    bullet.shapeColor ="red";
+
+  }
+
+
+}
+function collide(object1,object2){
+  if(object2.x-object1.x===object1.width/2 + object1.width/2-10)
+    {
+      object1.velocityX = 0;
+      object2.velocityX = 0;
+      damages();
+    }
+}
